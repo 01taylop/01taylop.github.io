390f613e3b9c275dbbef792e8b474772087c89f6
Patrick Taylor <patchfalcon+github@gmail.com>
2019-05-28 00:13:27 +0100

    Hello 👋🏻

--- js/main.js
+++ js/main.js
@@ -3,7 +3,83 @@ document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
 window.addEventListener("load", pageFullyLoaded, false);
 
 function theDomHasLoaded(e) {
-  console.log("theDomHasLoaded");
+  const greetings = [{
+    flag: "🇧🇷",
+    greeting: "Oi",
+  }, {
+    flag: "🇨🇳",
+    greeting: "Nǐ hǎo",
+  }, {
+    flag: "🇩🇰",
+    greeting: "Hej",
+  }, {
+    flag: "🇪🇪",
+    greeting: "Tere",
+  }, {
+    flag: "🇫🇮",
+    greeting: "Hei",
+  }, {
+    flag: "🇫🇷",
+    greeting: "Bonjour",
+  }, {
+    flag: "🇩🇪",
+    greeting: "Guten Tag",
+  }, {
+    flag: "🇮🇹",
+    greeting: "Ciao",
+  }, {
+    flag: "🇯🇵",
+    greeting: "Konnichiwa",
+  }, {
+    flag: "🇳🇱",
+    greeting: "Hallo",
+  }, {
+    flag: "🇵🇹",
+    greeting: "Olá",
+  }, {
+    flag: "🇷🇴",
+    greeting: "Salut",
+  }, {
+    flag: "🇪🇸",
+    greeting: "Hola",
+  }, {
+    flag: "🇸🇪",
+    greeting: "Hallå",
+  }]
+
+  const typeSpeed = 80
+  let previousI = 0
+  let previousGreeting = ""
+
+  function typeWelcome(flag, greeting) {
+    let i = 0;
+    function typewriter() {
+      if (previousI >= 0) {
+        document.getElementById("greeting").innerHTML = previousGreeting.substring(0, previousI);
+        if (previousI === 0) {
+          document.getElementById("flag").innerHTML = flag
+        }
+        previousI -= 1;
+        setTimeout(typewriter, typeSpeed);
+      } else if (i < greeting.length) {
+        document.getElementById("greeting").innerHTML = greeting.substring(0, i + 1);
+        i += 1;
+        setTimeout(typewriter, typeSpeed);
+      } else {
+        previousI = greeting.length
+        previousGreeting = greeting
+      }
+    }
+    typewriter()
+  }
+
+  setTimeout(function() {
+    typeWelcome("🇬🇧", "Hello")
+    setInterval(function() {
+      const { flag, greeting } = greetings[Math.floor(Math.random() * greetings.length)]
+      typeWelcome(flag, greeting)
+    }, 6000)
+  }, 1000);
 }
 
 function pageFullyLoaded(e) {
